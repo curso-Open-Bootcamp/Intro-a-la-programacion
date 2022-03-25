c884f27267db1b3c5f288d5eb8c93e3994e31562
edy527 <edgardobeltran527@gmail.com>
2022-03-25 18:26:12 -0300

    Ejercicios tema 4, estructurass de control, bucles

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,57 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        suma(10, 10, 5);
-        Coche miCoche = new Coche();
-        miCoche.AgregarPuerta();
-        System.out.println(miCoche.puertas);
 
-    }
+        //¿Es positivo o negativo? condicional if
+        int numeroIf = -1;
+        if(numeroIf < 0){
+            System.out.println("Es un número negativo");
+        }else{
+            System.out.println("Es positivo");
+        }
+        //FIN DEL PRIMER EJERCICIO
+        int numeroWhile =  -5;
+        while (numeroWhile < 3){
+            System.out.println(numeroWhile);
+            numeroWhile ++;
+        }
+        // SEGUNDO EJERCICIO (BUCLE) DO WHILE
+            do{
+                System.out.println("ESTE ES EL WHILE" + numeroWhile);
+            }while(numeroWhile < 3);
 
-    public static void suma(int a, int b, int c){
-       System.out.println(a + b + c);
+        // FIN DEL SEGUNDO EJERCICIO
+
+        // TERCER  EJERCICIO FOR
+
+        for (int numeroFor = 0; numeroFor <= 3; numeroFor = numeroFor+1){
+            System.out.println("Bucle for " + numeroFor);
+        }
+        // FIN DEL TERCER EJERCICIO
+
+        // CUARTO  EJERCICIO SWITCH
+        var estacion = "invierno";
+        switch (estacion){
+            case "verano":
+                System.out.println("Es verano");
+                break;
+            case "invierno":
+                System.out.println("Es invierno");
+                break;
+            case "primavera":
+                System.out.println("Es primavera");
+                break;
+            case "otoño":
+                System.out.println("Es otoño");
+                break;
+            default:
+                System.out.println("No sabemos en que estacion del año te encuentras");
+        }
+
+        // FIN DEL TERCER  EJERCICIO
     }
 }
 
-class Coche{
-    public int puertas = 0;
-
-    public void AgregarPuerta(){
-        this.puertas++;
-    }
-}
 
 
 
